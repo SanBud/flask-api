aa6c92c7e4d4e2cc1803bda49bd9f2d6467778e7
sbudni <santosh.budni@gmail.com>
2023-05-15 22:17:34 +0530

    minor change to trigger workflow

--- api/app.py
+++ api/app.py
@@ -8,7 +8,7 @@ app.register_blueprint(errors)
 
 @app.route("/")
 def index():
-    return Response("Hello, world!", status=200)
+    return Response("Hello, world! Adding workflow", status=200)
 
 
 @app.route("/custom", methods=["POST"])
